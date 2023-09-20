fef0c3d746215108ed48e64b8bfc07dc1efa0243
Liezl Mouton <liz.mouton1@gmail.com>
2023-09-20 17:29:18 +0200

    Made all of the variables uppercase

--- scripts.js
+++ scripts.js
@@ -1,35 +1,35 @@
 let DATE = 2050;
-let STATUS = "parent";
+let STATUS = "student";
 let COUNT = 0;
 
-if ((date = 2050)) {
+if ((DATE = 2050)) {
   console.log("January", "New Year’s Day");
   console.log("March", "Human Rights Day");
-  date = "April";
-  console.log(date, "Family Day");
-  console.log(date, "Freedom Day");
+  DATE = "April";
+  console.log(DATE, "Family Day");
+  console.log(DATE, "Freedom Day");
   COUNT = COUNT + 4;
 
-  if ((status = "student")) {
+  if ((STATUS = "student")) {
     console.log("June", "Youth Day");
     COUNT = COUNT + 1;
   }
 
   console.log("August", "Women’s Day");
   console.log("September", "Heritage Day");
-  date = "December";
-  console.log(date, "Day of Reconciliation");
+  DATE = "December";
+  console.log(DATE, "Day of Reconciliation");
   COUNT = COUNT + 3;
 
-  if ((status = "parent")) {
-    console.log(date, "Christmas Day");
+  if ((STATUS = "parent")) {
+    console.log(DATE, "Christmas Day");
     COUNT = COUNT + 1;
   }
 
-  console.log(date, "Day of Goodwill");
+  console.log(DATE, "Day of Goodwill");
   COUNT = COUNT + 1;
 }
 
-console.log("Your status is:", status);
-console.log("The year is:", date);
+console.log("Your status is:", STATUS);
+console.log("The year is:", DATE);
 console.log("The total holidays is:", COUNT);
